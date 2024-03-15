7d32abeea9653bd47bc27a5542abeab6bbaaec67
Boshre <142525404+Boshre@users.noreply.github.com>
2024-03-15 01:32:18 +0000

    projects list view

--- portfolio_website/portfolio_app/urls.py
+++ portfolio_website/portfolio_app/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('projects/',views.projectslist,name="projects")
 ]
